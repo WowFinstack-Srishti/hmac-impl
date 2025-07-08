ee40fd4943e4d85e4f4485c842d2042b02b75dd7
srishti-rao <srishtirao7@gmail.com>
2025-07-08 15:42:09 +0545

    Feature: set HmacConfig

--- src/main/java/com/wowfinstack/hmac/config/HmacConfig.java
+++ src/main/java/com/wowfinstack/hmac/config/HmacConfig.java
@@ -1,4 +1,14 @@
 package com.wowfinstack.hmac.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "hmac")
 public class HmacConfig {
+    private String secretKey;
 }
