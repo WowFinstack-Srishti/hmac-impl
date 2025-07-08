850442b3d52a189c184eba42744ad170697420ba
srishti-rao <srishtirao7@gmail.com>
2025-07-08 15:51:20 +0545

    Feature: set HmacService

--- src/main/java/com/wowfinstack/hmac/service/HmacService.java
+++ src/main/java/com/wowfinstack/hmac/service/HmacService.java
@@ -1,4 +1,6 @@
 package com.wowfinstack.hmac.service;
 
 public interface HmacService {
+    String generateSignature(String payload);
+    boolean isValidSignature(String payload, String providedSignature);
 }
