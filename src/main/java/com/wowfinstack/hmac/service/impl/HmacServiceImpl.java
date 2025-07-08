643b8efca62cca750471e5c27490010774b238fb
srishti-rao <srishtirao7@gmail.com>
2025-07-08 15:54:26 +0545

    Feature: set HmacServiceImpl

--- src/main/java/com/wowfinstack/hmac/service/impl/HmacServiceImpl.java
+++ src/main/java/com/wowfinstack/hmac/service/impl/HmacServiceImpl.java
@@ -1,4 +1,25 @@
 package com.wowfinstack.hmac.service.impl;
 
-public class HmacServiceImpl {
+import com.wowfinstack.hmac.config.HmacConfig;
+import com.wowfinstack.hmac.service.HmacService;
+import com.wowfinstack.hmac.util.HmacUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HmacServiceImpl implements HmacService {
+
+    @Autowired
+    private HmacConfig hmacConfig;
+
+    @Override
+    public String generateSignature(String payload) {
+        return HmacUtil.generateHmac(payload, hmacConfig.getSecretKey());
+    }
+
+    @Override
+    public boolean isValidSignature(String payload, String providedSignature) {
+        String calculated = generateSignature(payload);
+        return calculated.equals(providedSignature);
+    }
 }
