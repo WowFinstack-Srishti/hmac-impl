0303de20fc5c92c235bbe0ddbe7e0966a866c8af
srishti-rao <srishtirao7@gmail.com>
2025-07-08 16:10:05 +0545

    Feature: set HmacRequestFilter

--- src/main/java/com/wowfinstack/hmac/filter/HmacRequestFilter.java
+++ src/main/java/com/wowfinstack/hmac/filter/HmacRequestFilter.java
@@ -1,4 +1,33 @@
 package com.wowfinstack.hmac.filter;
 
-public class HmacRequestFilter {
+import com.wowfinstack.hmac.service.HmacService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class HmacRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private HmacService hmacService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String signature = request.getHeader("Sign");
+        if (signature == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Invalid HMAC Signature");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
